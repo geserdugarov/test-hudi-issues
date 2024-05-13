eb54e21046cbf8cf7262ef33844afda9f9dd95d3
Geser Dugarov <geserdugarov@gmail.com>
2024-05-13 14:30:01 +0700

    Description, .env support

--- HUDI-7717/combine-before-insert.py
+++ HUDI-7717/combine-before-insert.py
@@ -1,25 +1,37 @@
-from pyspark.sql import Row
-from pyspark.sql import SparkSession
+import os
+import shutil
+from pathlib import Path
+
+import dotenv
+import pyspark
+
+
+curr_dir = Path(__file__).parent.parent.absolute()
+spark_home = dotenv.dotenv_values(str(curr_dir / ".env"))['SPARK_HOME']
+os.environ['SPARK_HOME'] = str(spark_home)
+
+# prepare test related temporary directory
+tmp_dir_path = "/tmp/combine-before-insert"
+if os.path.exists(tmp_dir_path):
+    shutil.rmtree(tmp_dir_path)
 
 # Settings
-spark = SparkSession.builder.appName("combine-before-insert").getOrCreate()
+spark = (pyspark.sql.SparkSession.builder
+         .master("spark://linux-pc:7077")
+         .appName("combine-before-insert")
+         .config("spark.sql.warehouse.dir", "/tmp/spark-warehouse")
+         .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+         # .config("spark.kryo.registrator", "org.apache.spark.HoodieSparkKryoRegistrar")
+         # .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.hudi.catalog.HoodieCatalog")
+         # .config("spark.sql.hive.convertMetastoreParquet", "false")
+         .getOrCreate())
 
 # Data
-input_data = [
-    Row(
-        id=4,
-        value="foo",
-        ts=0,
-    ),
-    Row(
-        id=4,
-        value="bar",
-        ts=1,
-    ),
-]
+input_data = [pyspark.sql.Row(id=4, value="foo", ts=0),
+              pyspark.sql.Row(id=4, value="bar", ts=1)]
 df = spark.createDataFrame(input_data)
 
-# Example Hudi configs
+# # Example Hudi configs
 hudi_options = {
     "hoodie.table.name": "fake_name",
     "hoodie.datasource.write.table.name": "fake_name",
@@ -35,10 +47,13 @@ hudi_options = {
     "hoodie.datasource.write.operation": "bulk_insert",
 }
 
-PATH = "/tmp/combine-before-insert"
-df.write.format("hudi").options(**hudi_options).mode("overwrite").save(PATH)
+(df.write
+ .format("org.apache.hudi")
+ .options(**hudi_options)
+ .mode("overwrite")
+ .save(tmp_dir_path))
 
 # Should be 1 but prints 2
-print(pyspark.read.format("hudi").load(PATH).count())
+print(spark.read.format("hudi").load(tmp_dir_path).count())
 # Both rows exist
-print(pyspark.read.format("hudi").load(PATH).collect())
+print(spark.read.format("hudi").load(tmp_dir_path).collect())
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
+# Hudi issues check
 Repository for Python scripts to check [Hudi issues](https://github.com/apache/hudi/issues) reproducibility.
+
+## Setup
+PyCharm is used for running scrtipts. Spark is runned locally with one thread.
+1. [Download](https://spark.apache.org/downloads.html) corresponding Apache Spark version with Hadoop and extract to some folder, `SPARK_HOME`.
+2. Prepare Python virtual environment: 
+
+   - `python3 -m venv [env_name]`
+   - `source [env_name]/bin/activate`
+   - `pip install -r requirements.txt`
+
+3. Set `SPARK_HOME` in `.env` file.
+4. Add `pyspark.zip` and `py4j-*-src.zip` from `{SPARK_HOME}/python/lib/` into `Project` >> `Project Structure` in PyCharm.
+5. Build [Hudi project](https://github.com/apache/hudi/) and copy Spark bundle to `{SPARK_HOME}/jars/`.
+6. Start Spark cluster locally, `{SPARK_HOME}/sbin/start-all.sh`.
+
+## Checking
+Run corresponding script from configured PyCharm.
