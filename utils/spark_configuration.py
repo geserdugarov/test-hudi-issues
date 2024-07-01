27d779b7bc4ba07b10de7b872d797890a40a2df8
Geser Dugarov <geserdugarov@gmail.com>
2024-07-01 11:02:57 +0700

    Set level of logs

--- utils/spark_configuration.py
+++ utils/spark_configuration.py
@@ -7,13 +7,15 @@ from .configuration import configs
 
 def init_spark_env(app_name: str) -> pyspark.sql.SparkSession:
     _set_spark_home()
-    return (pyspark.sql.SparkSession.builder
-            .master(configs['SPARK_HOST_URL'])
-            .appName(app_name)
-            .config("spark.sql.warehouse.dir", configs['SPARK_WAREHOUSE_PATH'])
-            .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-            .config("spark.sql.extensions", "org.apache.spark.sql.hudi.HoodieSparkSessionExtension")  # to use Hudi specific syntax
-            .getOrCreate())
+    spark_session = (pyspark.sql.SparkSession.builder
+                     .master(configs['SPARK_HOST_URL'])
+                     .appName(app_name)
+                     .config("spark.sql.warehouse.dir", configs['SPARK_WAREHOUSE_PATH'])
+                     .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+                     .config("spark.sql.extensions", "org.apache.spark.sql.hudi.HoodieSparkSessionExtension")  # to use Hudi specific syntax
+                     .getOrCreate())
+    spark_session.sparkContext.setLogLevel("INFO")
+    return spark_session
 
 
 def _set_spark_home() -> None:
