50a29c145f7bdae2fbf483d6808d5b908a9cf457
Geser Dugarov <geserdugarov@gmail.com>
2024-09-18 18:48:10 +0700

    [HUDI-8173] Try to reproduce

---
+++ HUDI-8173/schema-compatibility-read-by-0.15.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+
+import utils
+
+
+# prepare environment
+script_name = os.path.basename(__file__)[:-3]
+spark = utils.init_spark_env(script_name)
+
+table_path = str(Path("/tmp/schema_compatibility"))
+df_load = spark.read.format("org.apache.hudi").load(table_path)
+for row in df_load.collect():
+    print(row)
+
+# need more description, the issue couldn't be reproduced in current 0.15
