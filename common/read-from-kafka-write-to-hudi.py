976e11f3e85dd6db123f34796d975f219bad633e
Geser Dugarov <geserdugarov@gmail.com>
2025-10-31 16:02:15 +0700

    Fix "read from Kafka, write to Hudi" scenario

--- common/read-from-kafka-write-to-hudi.py
+++ common/read-from-kafka-write-to-hudi.py
@@ -1,8 +1,7 @@
 import os
-from pathlib import Path
 
 import utils
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DecimalType, DateType
+from pyspark.sql import functions
 
 
 def prepare_offset_config(kafka_topic: str, partitions_num: int, set_offset: int) -> str:
@@ -20,9 +19,9 @@ spark = utils.init_spark_env(script_name)
 # Kafka read configuration
 kafka_brokers = "ip1:port1,ip2:port2"
 kafka_topic = "tpch_60kk"
-num_of_partitions = 8
-batch_size_from_1_partition = 88000
-num_of_batches = 10
+kafka_partitions_num = 8
+batch_size_from_1_partition = 100000
+num_of_batches = 3
 
 # Hudi configuration
 # If HDFS is used, then `fs.default.name` should be set in Spark's ./conf/core-site.xml, as:
@@ -36,32 +35,43 @@ table_name = "kafka_to_hudi"
 hudi_options = {
     "hoodie.table.name": f"{table_name}",
     "hoodie.datasource.write.table.type": "MERGE_ON_READ",
-    "hoodie.datasource.write.partitionpath.field": "l_quantity",
+    "hoodie.datasource.write.partitionpath.field": "",
     "hoodie.datasource.write.keygenerator.class": "org.apache.hudi.keygen.ComplexKeyGenerator",
     "hoodie.datasource.write.recordkey.field": "l_orderkey,l_linenumber",
     "hoodie.datasource.write.precombine.field": "l_linenumber",
+    "hoodie.index.type": "BUCKET",
+    "hoodie.index.bucket.engine": "SIMPLE",
+    "hoodie.bucket.index.num.buckets": f"{kafka_partitions_num}",
+    "hoodie.bucket.index.hash.field": "l_orderkey,l_linenumber",
     "hoodie.write.lock.provider": "org.apache.hudi.client.transaction.lock.InProcessLockProvider"
 }
 
 # data in Kafka topic is bytearray of csv string, this schema allows to parse this string
-schema_of_csv_str = StructType([
-    StructField("l_orderkey", IntegerType(), False),
-    StructField("l_partkey", IntegerType(), False),
-    StructField("l_suppkey", IntegerType(), False),
-    StructField("l_linenumber", IntegerType(), False),
-    StructField("l_quantity", DecimalType(), True),
-    StructField("l_extendedprice", DecimalType(), True),
-    StructField("l_discount", DecimalType(), True),
-    StructField("l_tax", DecimalType(), True),
-    StructField("l_returnflag", StringType(), True),
-    StructField("l_linestatus", StringType(), True),
-    StructField("l_shipdate", DateType(), True),
-    StructField("l_commitdate", DateType(), True),
-    StructField("l_receiptdate", DateType(), True),
-    StructField("l_shipinstruct", StringType(), True),
-    StructField("l_shipmode", StringType(), True),
-    StructField("l_comment", StringType(), True)
-])
+csv_schema = (
+    "l_orderkey INT, "
+    "l_partkey INT, "
+    "l_suppkey INT, "
+    "l_linenumber INT, "
+    "l_quantity DECIMAL, "
+    "l_extendedprice DECIMAL, "
+    "l_discount DECIMAL, "
+    "l_tax DECIMAL, "
+    "l_returnflag STRING, "
+    "l_linestatus STRING, "
+    "l_shipdate DATE, "
+    "l_commitdate DATE, "
+    "l_receiptdate DATE, "
+    "l_shipinstruct STRING, "
+    "l_shipmode STRING, "
+    "l_comment STRING"
+)
+csv_opts = {
+    "delimiter": ",",
+    "quote": '"',
+    "escape": "\\",
+    "mode": "FAILFAST",  # aborts the reading if any malformed data is found
+    "dateFormat": "yyyy-MM-dd"
+}
 
 # prepare Hudi table for write
 spark.sql(f"CREATE TABLE IF NOT EXISTS {table_name} ("
@@ -86,25 +96,26 @@ spark.sql(f"CREATE TABLE IF NOT EXISTS {table_name} ("
            "    type = 'MERGE_ON_READ',"
            "    primaryKey = 'l_orderkey,l_linenumber',"
            "    preCombineField = 'l_linenumber'"
-           ") PARTITIONED BY (l_quantity)"
-          f"  LOCATION '{hudi_table_path}'")
+           ") LOCATION '{hudi_table_path}'")
 
-# we read batches of (batch_size_from_1_partition * num_of_partitions) size, and write them to Hudi
+# we read batches of (batch_size_from_1_partition * kafka_partitions_num) size, and write them to Hudi
 for end_offset in range(batch_size_from_1_partition,
                         batch_size_from_1_partition * num_of_batches + 1,
                         batch_size_from_1_partition):
     start_offset = end_offset - batch_size_from_1_partition
-    df_csv = spark.read \
+    df = spark.read \
         .format("kafka") \
         .option("kafka.bootstrap.servers", kafka_brokers) \
         .option("subscribe", kafka_topic) \
-        .option("startingOffsets", prepare_offset_config(kafka_topic, num_of_partitions, start_offset)) \
-        .option("endingOffsets", prepare_offset_config(kafka_topic, num_of_partitions, end_offset)) \
+        .option("startingOffsets", prepare_offset_config(kafka_topic, kafka_partitions_num, start_offset)) \
+        .option("endingOffsets", prepare_offset_config(kafka_topic, kafka_partitions_num, end_offset)) \
         .load() \
-        .select("value")
+        .select(functions.col("value").cast("string").alias("csv_string")) \
+        .select(functions.from_csv("csv_string", csv_schema, csv_opts).alias("parsed")) \
+        .select("parsed.*") \
+        .repartition(kafka_partitions_num * 2, "l_orderkey", "l_linenumber")
 
-    df_curr = spark.read.csv(df_csv.rdd.map(lambda x: x.value.decode("utf-8")), schema=schema_of_csv_str)
-    df_curr.write \
+    df.write \
         .format("org.apache.hudi") \
         .options(**hudi_options) \
         .mode("append") \
