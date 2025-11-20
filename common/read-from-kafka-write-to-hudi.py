d52c12901859db605d9b24f14dc7f528d7b9d952
Geser Dugarov <geserdugarov@gmail.com>
2025-11-20 20:07:07 +0700

    Closer to real `read Kafka - write Hudi` pattern

--- common/read-from-kafka-write-to-hudi.py
+++ common/read-from-kafka-write-to-hudi.py
@@ -20,23 +20,32 @@ spark = utils.init_spark_env_for_hudi(script_name)
 kafka_brokers = "ip1:port1,ip2:port2"
 kafka_topic = "tpch_60kk"
 kafka_partitions_num = 8
-batch_size_from_1_partition = 1_500_000
-num_of_batches = 5
+# there are different max_offset in each topic partition
+# so, we limit read by corresponding min value
+min_of_end_offsets = 7_100_000
+# we read recs_per_partit_first records per partition first
+# and then recs_per_partit_ups records per partition
+# in all following (total_commits_num - 1) commits
+recs_per_partit_first = 4_500_000
+recs_per_partit_commit = 1_000_000
+total_commits_num = 4
 
-# Hudi configuration
+# Hudi write configuration
+table_name = "kafka_to_hudi_mor_upsert"
+table_type = "MERGE_ON_READ"
+operation_type = "upsert"
 # If HDFS is used, then `fs.default.name` should be set in Spark's ./conf/core-site.xml, as:
 #   <property>
 #       <name>fs.default.name</name>
 #       <value>hdfs://hdfs-namenode-ip:9000</value>
 #   </property>
 # For local write, use file:///tmp/some-path
-table_name = "kafka_to_hudi_mor_upsert"
 hudi_table_path = f"hdfs://hdfs-namenode-ip:9000/<user>/hudi/{table_name}"
 hudi_options = {
     "hoodie.table.name": f"{table_name}",
-    "hoodie.datasource.write.table.type": "MERGE_ON_READ",
+    "hoodie.datasource.write.table.type": f"{table_type}",
     # operation
-    "hoodie.datasource.write.operation": "upsert",
+    "hoodie.datasource.write.operation": f"{operation_type}",
     # partitioning
     "hoodie.datasource.write.partitionpath.field": "",
     "hoodie.datasource.write.keygenerator.class": "org.apache.hudi.keygen.ComplexKeyGenerator",
@@ -114,29 +123,38 @@ spark.sql(f"CREATE TABLE IF NOT EXISTS {table_name} ("
           "  l_comment STRING"
           ") USING hudi "
           "  TBLPROPERTIES ("
-          "    type = 'MERGE_ON_READ',"
+          f"   type = '{table_type}',"
           "    primaryKey = 'l_orderkey,l_linenumber',"
           "    preCombineField = 'l_linenumber'"
           ") LOCATION '{hudi_table_path}'")
 
-# we read batches of (batch_size_from_1_partition * kafka_partitions_num) size, and write them to Hudi
-for end_offset in range(batch_size_from_1_partition,
-                        batch_size_from_1_partition * num_of_batches + 1,
-                        batch_size_from_1_partition):
-    start_offset = end_offset - batch_size_from_1_partition
-    df = spark.read \
-        .format("kafka") \
-        .option("kafka.bootstrap.servers", kafka_brokers) \
-        .option("subscribe", kafka_topic) \
-        .option("startingOffsets", prepare_offset_config(kafka_topic, kafka_partitions_num, start_offset)) \
-        .option("endingOffsets", prepare_offset_config(kafka_topic, kafka_partitions_num, end_offset) if end_offset < 5_500_000 else "latest") \
-        .load() \
-        .select(functions.col("value").cast("string").alias("csv_string")) \
-        .select(functions.from_csv("csv_string", csv_schema, csv_opts).alias("parsed")) \
-        .select("parsed.*")
+# read Kafka topic partitions in batches by configured offsets,
+# and write to Hudi directly
+end_offset = 0
+for commit_id in range(total_commits_num):
+    if commit_id == 0:
+        # first the biggest commit
+        start_offset = 0
+        end_offset = recs_per_partit_first
+    else:
+        # following commits
+        start_offset = end_offset
+        end_offset += recs_per_partit_commit
+    df = (spark.read.format("kafka")
+          .option("kafka.bootstrap.servers", kafka_brokers)
+          .option("subscribe", kafka_topic)
+          .option("startingOffsets",
+                  prepare_offset_config(kafka_topic, kafka_partitions_num, start_offset))
+          .option("endingOffsets",
+                  prepare_offset_config(kafka_topic, kafka_partitions_num, end_offset)
+                  if end_offset < min_of_end_offsets
+                  else "latest")
+          .load()
+          .select(functions.col("value").cast("string").alias("csv_string"))
+          .select(functions.from_csv("csv_string", csv_schema, csv_opts).alias("parsed"))
+          .select("parsed.*"))
 
-    df.write \
-        .format("org.apache.hudi") \
-        .options(**hudi_options) \
-        .mode("append") \
-        .save(hudi_table_path)
+    (df.write.format("hudi")
+     .options(**hudi_options)
+     .mode("append")
+     .save(hudi_table_path))
